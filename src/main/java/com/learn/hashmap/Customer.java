f4d82453e9b2aca35451a476372be3cf0d41e8b9
Sanjaya Halwala <sanjayahalwala@gmail.com>
2024-11-23 15:31:26 +0530

    Hash map related codes,task updated

--- src/main/java/com/learn/hashmap/Customer.java
+++ src/main/java/com/learn/hashmap/Customer.java
@@ -1,2 +1,14 @@
-package com.learn.hashMap;public class Customer {
+package com.learn.hashmap;
+
+public class Customer {
+    private final String id;
+
+    public Customer(String cusId) {
+        this.id = cusId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
 }
--- src/main/java/com/learn/hashmap/Order.java
+++ src/main/java/com/learn/hashmap/Order.java
@@ -1,2 +1,38 @@
-package com.learn.hashMap;public class Order {
+package com.learn.hashmap;
+
+class Order {
+    private final String id;
+    private final String type;
+    private String status;
+
+    public Order(String orderId, String orderType, String orderStatus) {
+        this.id = orderId;
+        this.type = orderType;
+        this.status = orderStatus;
+    }
+
+    public String getOrderId() {
+        return id;
+    }
+
+    public String getOrderType() {
+        return type;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id='" + id + '\'' +
+                ", type='" + type + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
 }
