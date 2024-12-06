56211e9be703be6e57097b36894a34ecef3ba3e2
Sanjaya Halwala <sanjayahalwala@gmail.com>
2024-12-06 23:28:41 +0530

    functional programming question updates

--- src/main/java/com/learn/questions/funcprog/q2/Demo.java
+++ src/main/java/com/learn/questions/funcprog/q2/Demo.java
@@ -5,12 +5,16 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Demo {
-      public static void main(String[] args) {
-          List<Integer> numbers = Arrays.asList(1,2,3,4,5);
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
 
-          List<Integer> out = numbers.stream().filter(num -> num%2 != 0).map(n->n*n).collect(Collectors.toList());
-          System.out.println(out);
+        List<Integer> out = numbers.stream()
+                .filter(num -> num % 2 != 0)
+                .map(n -> n * n)
+                .collect(Collectors.toList());
 
-      }
+        System.out.println(out);
+
+    }
 
 }
