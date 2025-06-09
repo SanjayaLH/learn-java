6f4e7904d36b122aecfef4dcd7cc0b445a64bd37
Kalpa Senanayake <kalpa.senanayake@gmail.com>
2025-06-09 10:48:26 +1000

    Adding sckelaton of the event driven arch

---
+++ event-driven-arch/src/main/java/com/hal/event_driven_arch/EventDrivenArchApplication.java
@@ -0,0 +1,13 @@
+package com.hal.event_driven_arch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDrivenArchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventDrivenArchApplication.class, args);
+	}
+
+}
---
+++ event-driven-arch/src/test/java/com/hal/event_driven_arch/EventDrivenArchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hal.event_driven_arch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventDrivenArchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
